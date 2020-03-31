66117041cff247d03ff4b545dfb8cfeb97531bec
Paul TONDEREAU <paul.tondereau@soprasteria.com>
2020-03-31 22:11:29 +0200

    Fin filter cat

--- front-angular/src/app/home/home.component.ts
+++ front-angular/src/app/home/home.component.ts
@@ -12,73 +12,74 @@ import { FormControl, NgModel } from '@angular/forms';
 
 export class HomeComponent implements OnInit {
 
-  listSurveys:Survey[]=[];
+  listSurveys: Survey[] = [];
   listSurveysSubscription: Subscription;
 
   sortedBy: String;
   isSortingPopupOpen: Boolean = false;
 
-  sortKey='time';
-  sortingOptions=[
+  sortKey = 'time';
+  sortingOptions = [
     {
-      value:"time",
-      display:"Date de publication"
+      value: "time",
+      display: "Date de publication"
     },
     {
-      value:"participations",
-      display:"Nombre de participants"
+      value: "participations",
+      display: "Nombre de participants"
     },
     {
-      value:"likes",
-      display:"Nombre de likes"
-    },
-  ]
-
-  categorieKey= 'all';
-  categories=[
-    {
-      value:"all",
-      display:"Toutes"
-    },
-    {
-      value:"love",
-      display:"Amour"
-    },
-    {
-      value:"society",
-      display:"Société"
-    },
-    {
-      value:"useless",
-      display:"Inutile"
-    },
-    {
-      value:"sport",
-      display:"Sport"
-    },
-    {
-      value:"news",
-      display:"Actualité"
-    },
-    {
-      value:"art",
-      display:"Art"
-    },
-    {
-      value:"life",
-      display:"Vie quotidienne"
-    },
-    {
-      value:"music",
-      display:"Musique"
-    },
-    {
-      value:"sex",
-      display:"Sexe"
+      value: "likes",
+      display: "Nombre de likes"
     },
   ]
 
-  constructor(private surveyManagementService : SurveyManagementService) {}
+  categorieKey = 'all';
+  categorieFiltered = 'all';
+  categories = [
+    {
+      value: "all",
+      display: "Toutes"
+    },
+    {
+      value: "love",
+      display: "Amour"
+    },
+    {
+      value: "society",
+      display: "Société"
+    },
+    {
+      value: "useless",
+      display: "Inutile"
+    },
+    {
+      value: "sport",
+      display: "Sport"
+    },
+    {
+      value: "news",
+      display: "Actualité"
+    },
+    {
+      value: "art",
+      display: "Art"
+    },
+    {
+      value: "life",
+      display: "Vie quotidienne"
+    },
+    {
+      value: "music",
+      display: "Musique"
+    },
+    {
+      value: "sex",
+      display: "Sexe"
+    },
+  ]
+
+  constructor(private surveyManagementService: SurveyManagementService) { }
 
   ngOnInit() {
     this.surveyManagementService.getAllSurveys();
@@ -91,19 +92,18 @@ export class HomeComponent implements OnInit {
     this.surveyManagementService.emitListSurveysSubject();
   }
 
-  openOrClosePopup() {
-    this.isSortingPopupOpen = !this.isSortingPopupOpen;
-  }
-
   changeSortBy(key: string) {
-    if (key != this.sortedBy) {
+    if (!key || key != this.sortedBy) {
       this.surveyManagementService.sortSurveys(key);
       this.sortedBy = key;
       this.isSortingPopupOpen = false;
     }
   }
 
-  categorieFilter(keyCat:string){
-    this.surveyManagementService.filter(keyCat);
+  categorieFilter(keyCat: string) {
+    if (keyCat != this.categorieFiltered) {
+      this.surveyManagementService.filterByCat(keyCat);
+      this.categorieFiltered = keyCat;
+    }
   }
 }
--- front-angular/src/app/home/surveyManagement.service.ts
+++ front-angular/src/app/home/surveyManagement.service.ts
@@ -10,135 +10,162 @@ import { Subject } from 'rxjs';
 export class SurveyManagementService {
 
   listSurveys: Survey[] = [];
-  listSurveysSubject= new Subject<Survey[]>();
+  listSurveysSubject = new Subject<Survey[]>();
 
   oneSurvey: Survey;
-  oneSurveySubject= new Subject<Survey>();
+  oneSurveySubject = new Subject<Survey>();
 
-  constructor(private httpClient : HttpClient, private router : Router, private cookieService: CookieService) {}
+  keySort: string = 'time';
 
-  getAllSurveys(){
+  constructor(private httpClient: HttpClient, private router: Router, private cookieService: CookieService) { }
+
+  getAllSurveys() {
     this.httpClient.get<any>(environment.apiUrl + '/surveys').subscribe(
       (response) => {
-          this.listSurveys = [];
-          console.log('Found ' + response.length + ' offers');
-          response.forEach((surveyJson) => {
-              const survey = new Survey();
-              survey.fromHashMap(surveyJson);
-              this.listSurveys.push(survey);
-          });
-          this.emitListSurveysSubject();
-      },
-      (error) => {
-          console.log('Erreur ! : ' + error);
-      }
-    );
-  }
-
-  getOneSurvey(idSharedSurvey:String){
-    this.httpClient.post<any>(environment.apiUrl + '/surveys/getById',{"idSurvey":idSharedSurvey}).subscribe(
-      (response) => {
+        this.listSurveys = [];
+        console.log('Found ' + response.length + ' surveys');
+        response.forEach((surveyJson) => {
           const survey = new Survey();
-          survey.fromHashMap(response);
-          this.oneSurvey=survey
-          this.emitOneSurveySubject();
+          survey.fromHashMap(surveyJson);
+          this.listSurveys.push(survey);
+        });
+        this.emitListSurveysSubject();
       },
       (error) => {
-          console.log('Erreur ! : ' + error);
+        console.log('Erreur ! : ' + error);
       }
     );
   }
 
-  sortSurveys(key:string){
-      if (key == "time") {
-          this.listSurveys.sort(function (a: Survey, b: Survey) {
-              return +new Date(a.date).getTime() - +new Date(b.date).getTime();
-          });
-      } else if (key == "likes") {
-          this.listSurveys.sort(function (a: Survey, b: Survey) {
-              return +a.likes - b.likes;
-          });
-      } else if (key == "participations") {
-          this.listSurveys.sort(function (a: Survey, b: Survey) {
-              return +a.participations - b.participations;
-          });
+  getOneSurvey(idSharedSurvey: String) {
+    this.httpClient.post<any>(environment.apiUrl + '/surveys/getById', { "idSurvey": idSharedSurvey }).subscribe(
+      (response) => {
+        const survey = new Survey();
+        survey.fromHashMap(response);
+        this.oneSurvey = survey
+        this.emitOneSurveySubject();
+      },
+      (error) => {
+        console.log('Erreur ! : ' + error);
       }
-      this.emitListSurveysSubject();
+    );
   }
 
-  postSurvey(newSurvey : Survey){
+  sortSurveys(key: string) {
+    this.keySort = key;
+    if (key == "time") {
+      this.listSurveys.sort(function (a: Survey, b: Survey) {
+        return +new Date(a.date).getTime() - +new Date(b.date).getTime();
+      });
+    } else if (key == "likes") {
+      this.listSurveys.sort(function (a: Survey, b: Survey) {
+        return +a.likes - b.likes;
+      });
+    } else if (key == "participations") {
+      this.listSurveys.sort(function (a: Survey, b: Survey) {
+        return +a.participations - b.participations;
+      });
+    }
+    this.emitListSurveysSubject();
+  }
+
+  filterByCat(keyCat: string) {
+    console.log(keyCat)
+    if (keyCat == 'all') {
+      this.getAllSurveys();
+      return;
+    }
+    this.httpClient.post<any>(environment.apiUrl + '/surveys/filteredByCat', { "cat": keyCat }).subscribe(
+      (response) => {
+        this.listSurveys = [];
+        console.log('Found ' + response.length + ' surveys');
+        response.forEach((surveyJson) => {
+          const survey = new Survey();
+          survey.fromHashMap(surveyJson);
+          this.listSurveys.push(survey);
+        });
+        this.sortSurveys(this.keySort)
+        this.emitListSurveysSubject();
+      },
+      (error) => {
+        console.log('Erreur ! : ' + error);
+      }
+    );
+  }
+
+  postSurvey(newSurvey: Survey) {
     this.httpClient.post<Survey>(environment.apiUrl + '/surveys/post', newSurvey).subscribe(
       (response) => {
-          console.log('Sondage ajouté avec succès')
-          this.listSurveys.push(newSurvey);
-          this.router.navigate(['home']);
+        console.log('Sondage ajouté avec succès')
+        this.listSurveys.push(newSurvey);
+        this.router.navigate(['home']);
       },
       (error) => {
-          console.log('Erreur ! : ' + error);
+        console.log('Erreur ! : ' + error);
       }
     );
   }
 
-  vote(survey:Survey, option:SurveyOption){
+  vote(survey: Survey, option: SurveyOption) {
     let user = this.cookieService.get("WS-user")
-    this.httpClient.post<any>(environment.apiUrl + '/surveys/vote', {"surveyId":survey.id, "option":option, "user": user}).subscribe(
+    this.httpClient.post<any>(environment.apiUrl + '/surveys/vote', { "surveyId": survey.id, "option": option, "user": user }).subscribe(
       (response) => {
-          console.log('Vote ajouté avec succès');
-          const survey = new Survey();
-          survey.fromHashMap(response);
-          console.log(survey)
-          this.listSurveys.forEach((oneSurvey)=>{
-            if(oneSurvey.id==survey.id){
-              oneSurvey.options = survey.options;
-              this.emitListSurveysSubject();
-            }
-          })
-          //On met en session le vote
-          let mapVote = JSON.parse(this.cookieService.get('WS-mapVote'));
-          mapVote[""+survey.id]=option.text;
-          this.cookieService.set('WS-mapVote', JSON.stringify(mapVote), 365);
+        console.log('Vote ajouté avec succès');
+        const survey = new Survey();
+        survey.fromHashMap(response);
+        console.log(survey)
+        this.listSurveys.forEach((oneSurvey) => {
+          if (oneSurvey.id == survey.id) {
+            oneSurvey.options = survey.options;
+            this.emitListSurveysSubject();
+          }
+        })
+        //On met en session le vote
+        let mapVote = JSON.parse(this.cookieService.get('WS-mapVote'));
+        mapVote["" + survey.id] = option.text;
+        this.cookieService.set('WS-mapVote', JSON.stringify(mapVote), 365);
       },
       (error) => {
-          console.log('Erreur ! : ' + error);
+        console.log('Erreur ! : ' + error);
       }
     );
   }
 
-  likeOrDislike(survey:Survey, isLike:boolean){
-    this.httpClient.post<any>(environment.apiUrl + '/surveys/likedislike', {"surveyId":survey.id, "isLike":isLike}).subscribe(
+  likeOrDislike(survey: Survey, isLike: boolean) {
+    this.httpClient.post<any>(environment.apiUrl + '/surveys/likedislike', { "surveyId": survey.id, "isLike": isLike }).subscribe(
       (response) => {
-          console.log('Like ou dislike ajouté avec succès');
-          //On met le like en session
-          let mapLike = JSON.parse(this.cookieService.get('WS-mapLike'));
-          mapLike[""+survey.id]=isLike;
-          this.cookieService.set('WS-mapLike', JSON.stringify(mapLike), 365);
+        console.log('Like ou dislike ajouté avec succès');
+        //On met le like en session
+        let mapLike = JSON.parse(this.cookieService.get('WS-mapLike'));
+        mapLike["" + survey.id] = isLike;
+        this.cookieService.set('WS-mapLike', JSON.stringify(mapLike), 365);
       },
       (error) => {
-          console.log('Erreur ! : ' + error);
+        console.log('Erreur ! : ' + error);
       }
     );
   }
 
-  addWarning(survey:Survey){
-    this.httpClient.post<any>(environment.apiUrl + '/surveys/addwarning', {"surveyId":survey.id}).subscribe(
+  addWarning(survey: Survey) {
+    this.httpClient.post<any>(environment.apiUrl + '/surveys/addwarning', { "surveyId": survey.id }).subscribe(
       (response) => {
-          console.log('Warning ajouté avec succès');
-          //On met le warning en session
-          let listWarning = this.cookieService.get('WS-listWarning');
-          listWarning+=","+survey.id
-          this.cookieService.set('WS-listWarning', listWarning, 365);
+        console.log('Warning ajouté avec succès');
+        //On met le warning en session
+        let listWarning = this.cookieService.get('WS-listWarning');
+        listWarning += "," + survey.id
+        this.cookieService.set('WS-listWarning', listWarning, 365);
       },
       (error) => {
-          console.log('Erreur ! : ' + error);
+        console.log('Erreur ! : ' + error);
       }
     );
   }
 
-  emitListSurveysSubject(){
-    this.listSurveysSubject.next(this.listSurveys.length!==0 ? this.listSurveys.slice() : []);
+  emitListSurveysSubject() {
+    this.listSurveysSubject.next(this.listSurveys.length !== 0 ? this.listSurveys.slice() : []);
   }
 
-  emitOneSurveySubject(){
+  emitOneSurveySubject() {
     this.oneSurveySubject.next(this.oneSurvey);
   }
 }
